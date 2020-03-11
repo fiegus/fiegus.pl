2dd2a7282607cc4ee127b08c33bdc595d0503167
fiegus <piotrfiega@gmail.com>
2020-03-11 22:05:43 +0100

    react init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './js.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+        <p>© 2020 Piotr Fiega</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
